b23676ba2dc298567d7ed7db4239601806634269
Eric <tao.china@gmail.com>
2014-04-25 18:30:48 +0800

    Initial Commit

---
+++ shudong/SDViewController.m
@@ -0,0 +1,29 @@
+//
+//  SDViewController.m
+//  shudong
+//
+//  Created by Eric Tao on 4/25/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import "SDViewController.h"
+
+@interface SDViewController ()
+
+@end
+
+@implementation SDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ shudong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  shudong
+//
+//  Created by Eric Tao on 4/25/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
