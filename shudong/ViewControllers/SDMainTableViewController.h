3968524acc76edb819df3151173045a755398de0
Eric <tao.china@gmail.com>
2014-04-25 21:58:12 +0800

    avoscloudui

---
+++ shudong/ViewControllers/SDMainTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SDMainTableViewController.h
+//  shudong
+//
+//  Created by Eric Tao on 4/25/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVOSCloudUI/AVQueryTableViewController.h>
+#import <AVOSCloud/AVOSCloud.h>
+
+
+@interface SDMainTableViewController : AVQueryTableViewController
+
+@end
