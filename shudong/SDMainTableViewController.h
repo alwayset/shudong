2bc36f01ae027b2b6c25a017a3a269280eac6907
Eric <tao.china@gmail.com>
2014-04-26 15:14:54 +0800

    added main vc

---
+++ shudong/SDMainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDMainTableViewController.h
+//  shudong
+//
+//  Created by Eric Tao on 4/26/14.
+//  Copyright (c) 2014 1xiustudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDMainTableViewController : UITableViewController
+
+@end
