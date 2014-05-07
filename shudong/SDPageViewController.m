34188d917e4538b5619454be34827e4ad4e44a62
Eric <tao.china@gmail.com>
2014-05-07 22:56:23 +0800

    page control

--- shudong/SDPageViewController.m
+++ shudong/SDPageViewController.m
@@ -35,6 +35,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setViewControllers:(NSArray *)viewControllers direction:(UIPageViewControllerNavigationDirection)direction animated:(BOOL)animated completion:(void (^)(BOOL))completion {
+    
+}
+
 /*
 #pragma mark - Navigation
 
