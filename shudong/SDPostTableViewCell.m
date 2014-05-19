ced5eb6f0b1bd6154e3efd175365f136df880d48
Eric <tao.china@gmail.com>
2014-05-19 20:40:17 +0800

    改回来了

--- shudong/SDPostTableViewCell.m
+++ shudong/SDPostTableViewCell.m
@@ -34,8 +34,8 @@
 - (void)awakeFromNib
 {
     // Initialization code
-    titleLabel.font = [UIFont fontWithName:FONT_1 size:17.0];
-    text.font = [UIFont fontWithName:FONT_1 size:14.0];
+    titleLabel.font = [UIFont fontWithName:FONT_1 size:18.0];
+    text.font = [UIFont fontWithName:FONT_1 size:16.0];
     containerView.layer.shadowColor = [UIColor darkGrayColor].CGColor;
     containerView.layer.shadowOpacity = 0.7f;
     containerView.layer.shadowOffset = CGSizeMake(2.0f, 2.0f);
